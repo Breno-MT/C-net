54acde060b80a82da31d0b810aceac12ce419213
Breno-MT <brenomtdev@gmail.com>
2025-05-02 20:56:28 -0300

    FEAT: Declare Variable

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        // declare a (string) variable
+        string myFriendsName;
+        myFriendsName = "John M.";
+        Console.WriteLine(myFriendsName);
+
+        string myFriendsName2 = "Arthur M.";
+        Console.WriteLine(myFriendsName2);
     }
 }
