f4e00a1222ef475082b8198598ffa4a3541e6982
Breno-MT <brenomtdev@gmail.com>
2025-05-02 20:58:07 -0300

    REFACT: renamed Program.cs > Variable.cs

---
+++ ConsoleApp1/Variable.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+class Variable
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // declare a (string) variable
+        string myFriendsName;
+        myFriendsName = "John M.";
+        Console.WriteLine(myFriendsName);
+
+        string myFriendsName2 = "Arthur M.";
+        Console.WriteLine(myFriendsName2);
+    }
+}
