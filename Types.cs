1c30206760cde371b62cb5c68b18da2c77a3129b
Breno-MT <brenomtdev@gmail.com>
2025-05-02 21:27:40 -0300

    FEAT: added more types of data (byte, float, long, etc...)

--- Types.cs
+++ Types.cs
@@ -13,6 +13,20 @@ namespace Name
             string myName = "Breno";
             // store only a single char
             char at = "@";
+
+            /*
+            byte -> 0 to 255
+            signed byte -> -128 to 127
+            unsigned byte -> 0 to 255
+            short -> -32,768 to 32,767
+            signed short -> -32,768 to 32,767
+            unsigned short -> 0 to 65,535
+            long -> too much bro
+            signed long -> same bro
+            unsigned long -> from 0 to too much bro
+            float -> up to 7 digits e.g: 9.1234567F is a float
+            double -> e.g: 0.42d is a double
+            */
             
 
             // Types of Comments
