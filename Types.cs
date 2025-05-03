8f65687b5e4a6bbbcffc9ff2ec964fe8459dcc88
Breno-MT <brenomtdev@gmail.com>
2025-05-02 21:13:45 -0300

    FEAT: added Types

---
+++ Types.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Name
+{
+    class Types
+    {
+        static void Main(string[] args)
+        {
+            int IamNumber5 = 5;
+            float pi = 3.1415;
+            bool isBoolean = true;
+            string myName = "Breno";
+            // store only a single char
+            char at = "@";
+            
+        }
+    }
+}
