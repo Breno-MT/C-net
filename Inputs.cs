a170b7debc7fc0ac11d6bbb2386be8af3a689a12
Breno-MT <brenomtdev@gmail.com>
2025-05-02 21:09:43 -0300

    FEAT: added Inputs.cs

---
+++ Inputs.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp1;
+
+class Inputs {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, type your name:");
+        string userInput = Console.ReadLine();
+        Console.WriteLine("Nice to meet you, " + userInput + "!");
+    }
+}
