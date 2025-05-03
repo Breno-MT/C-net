3feb1fbe662aa2ccef4d15f9a4af53fc17d25f07
Breno-MT <brenomtdev@gmail.com>
2025-05-02 21:17:10 -0300

    FEAT: added types of Comments

--- Types.cs
+++ Types.cs
@@ -7,12 +7,27 @@ namespace Name
         static void Main(string[] args)
         {
             int IamNumber5 = 5;
+            /* Comments are important */
             float pi = 3.1415;
             bool isBoolean = true;
             string myName = "Breno";
             // store only a single char
             char at = "@";
             
+
+            // Types of Comments
+            // single line -> //
+            //singleline yay
+            
+            // multiline -> /**/
+            /*Hello, World!
+            That's... well.
+            */
+
+            // xml -> ///
+            ///<summary>
+            //help
+            ///</summary>
         }
     }
 }
