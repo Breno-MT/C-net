7770829d0edeed2c23abead4f72911bb8239f96f
Breno-MT <brenomtdev@gmail.com>
2025-05-04 20:58:56 -0300

    FEAT: added Array

---
+++ Arrays.cs
@@ -0,0 +1,7 @@
+// Declare an array (must say how many numbers inside that array, in this case will be 5)
+int[] myIntArray = new int[5];
+
+// Or declare it with the values already in it
+int[] myIntArray2 = [5,12,55,16,19];
+
+string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
