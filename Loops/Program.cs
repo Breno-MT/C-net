dc506f7fbed54d06ab6af8c764f96266a62b93a7
Breno-MT <brenomtdev@gmail.com>
2025-05-04 20:59:04 -0300

    FEAT: added forEach

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,16 +6,24 @@ class Program
     {
         // This Loop will count down from 10 to 1
         // To include 0 on this list, just change to: i >= 0
-        for (int i = 10; i > 0; i--) {
-            Console.WriteLine(i);
-            // Makes the system wait 1sec before decrementing again
-            // Thread.Sleep(1000);
-        }
+        // for (int i = 10; i > 0; i--) {
+        //     Console.WriteLine(i);
+        //     // Makes the system wait 1sec before decrementing again
+        //     // Thread.Sleep(1000);
+        // }
 
-        int counter = 0;
-        while (counter < 11) {
-            Console.WriteLine(counter);
-            counter++;
+        // int counter = 0;
+        // while (counter < 11) {
+        //     Console.WriteLine(counter);
+        //     counter++;
+        // }
+
+        // Foreach is the best option when you want to see, manipulate, etc... data inside an array of objects (or just an array like this)
+        string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        foreach (var day in weekDays)
+        {
+            Console.WriteLine(day);
         }
     }
 }
