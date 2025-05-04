4b073c6937e42a83ec3af4200d8d930e9188ff10
Breno-MT <brenomtdev@gmail.com>
2025-05-04 14:58:54 -0300

    FEAT: added a few more tricks to the calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+double number1 = 0.0;
+double number2 = 0.0;
+
+Console.WriteLine("Enter a number below:");
+string userInput = Console.ReadLine();
+number1 = double.Parse(userInput, CultureInfo.InvariantCulture);
+
+Console.WriteLine("Good, now a second number:");
+string userInput2 = Console.ReadLine();
+number2 = double.Parse(userInput2, CultureInfo.InvariantCulture);       // Some countries use comma instead of dot
+
+double sum = number1 + number2;
+sum = Math.Round(sum, 2);
+
+Console.WriteLine($"The sum of this two numbers are: {sum}");
+
