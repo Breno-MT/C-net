0d8ac47b38b4fcb31816b616bb86291eef5f07c4
Breno-MT <brenomtdev@gmail.com>
2025-05-04 17:07:56 -0300

    FEAT: string format and manipulation

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = 10;
+double price = 19.25;
+string name = "Breno";
+
+// Interpolation
+Console.WriteLine($"The number is {num}");
+
+// String concatenation
+Console.WriteLine("The number is " + num);
+
+// String Formatting
+Console.WriteLine("The number is {0}, and the price is {1}, and the name is {2}", num, price, name);
+
+Console.WriteLine("Hello\n \tWorld!");
