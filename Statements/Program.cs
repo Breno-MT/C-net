3ec4bc07a06cd73afd893cb7275c42a6856b0a85
Breno-MT <brenomtdev@gmail.com>
2025-05-04 19:27:05 -0300

    FEAT: added Statements and Operators

---
+++ Statements/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Operators
+// It has an img with all on it, cheat sheet actually
+
+// Switch Case
+int day = 3;
+switch (day) {
+    case 1:
+        Console.WriteLine("Monday!");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday!");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday!");
+        break;
+    default:
+        Console.WriteLine("Friday!!!");
+        break;
+}
+
+// If-elseif-else Statement
+bool isTrue = true;
+
+if (isTrue) {
+    Console.WriteLine("It is true!");
+}
+
+else if (!isTrue) {
+    Console.WriteLine("Is actually false!");
+}
+
+else {
+    Console.WriteLine("What did you do....");
+}
+
+
