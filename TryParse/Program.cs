1a26bb8d7b867de835d78b84bc95d377653802d8
Breno-MT <brenomtdev@gmail.com>
2025-05-04 20:05:16 -0300

    REFACT: fixed semicolon error, added guess the number (random)

--- TryParse/Program.cs
+++ TryParse/Program.cs
@@ -6,7 +6,7 @@ int num1 = int.Parse(userInput);
 bool isNumber = int.TryParse(userInput, out num1);
 
 if (isNumber) {
-    Console.WriteLine("It is a number.")
+    Console.WriteLine("It is a number.");
 }
 
 else {
@@ -15,3 +15,19 @@ else {
 
 num1++;
 Console.WriteLine("User entered number +1 " + num1);
+
+// random class
+Random random = new Random();
+// This will get us a random number between 1 and 10
+int randomNumber = random.Next(1,11);
+Console.WriteLine("Guess the number bro (1-10):");
+int userAnswer1 = int.Parse(Console.ReadLine());
+
+// You can debug it to see the number that has been generated hehe
+if (userAnswer1 == randomNumber) {
+    Console.WriteLine("Nice guess! It's correct! The number was {0}!", randomNumber);
+}
+
+else {
+    Console.WriteLine("Well, you tried");
+}
