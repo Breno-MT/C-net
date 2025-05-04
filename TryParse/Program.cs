668c3b4b780117eabacab5f0e15cf9e8eeedc378
Breno-MT <brenomtdev@gmail.com>
2025-05-04 19:56:04 -0300

    FEAT: added Try Parse

---
+++ TryParse/Program.cs
@@ -0,0 +1,17 @@
+﻿// try Parse
+Console.WriteLine("Gimme a number: ");
+string userInput = Console.ReadLine();
+int num1 = int.Parse(userInput);
+
+bool isNumber = int.TryParse(userInput, out num1);
+
+if (isNumber) {
+    Console.WriteLine("It is a number.")
+}
+
+else {
+    Console.WriteLine("You tried, but enter a number next time.");
+}
+
+num1++;
+Console.WriteLine("User entered number +1 " + num1);
