491eb787f66196de44f683047e2667e63f273843
Breno-MT <brenomtdev@gmail.com>
2025-05-06 09:50:34 -0300

    FEAT: added Customer class, constructors

---
+++ ClassesApp/Customer.cs
@@ -0,0 +1,32 @@
+namespace ClassesApp
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+        
+
+        // Default Constructor
+        public Customer()
+        {
+            Name = "Guest";
+            Address = "Unknown";
+            ContactNumber = "None";
+        }
+
+        
+        // Custom Constructor
+        public Customer(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- ClassesApp/Program.cs
+++ ClassesApp/Program.cs
@@ -4,14 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Car bmw = new Car("318i", "Bmw", true);
-        Car audi = new Car("RS6", "Audi", false);
+        // Car bmw = new Car("318i", "Bmw", true);
+        // Car audi = new Car("RS6", "Audi", false);
 
-        Console.WriteLine("Please type the Brand name:");
-        // Setting Brand
-        // audi.Brand = Console.ReadLine();
+        // Console.WriteLine("Please type the Brand name:");
+        // // Setting Brand
+        // // audi.Brand = Console.ReadLine();
 
-        // Getting Brand
-        Console.WriteLine("You entered: {0}", audi.Brand);
+        // // Getting Brand
+        // Console.WriteLine("You entered: {0}", audi.Brand);
+
+        Customer bento = new Customer("Bento");
+        Customer jota = new Customer("Jotinha", "Xique-xique Bahia", "02913102231");
+
+        // Default Customer
+        Customer guest = new Customer();
+
+        Console.WriteLine(bento.Name);
+        Console.WriteLine("Hello! This is {0} and his number {1}", jota.Name, jota.ContactNumber);
+        Console.WriteLine("Hello, {0}!", guest.Name);
+        Console.Write("Type your name: ");
+        guest.Name = Console.ReadLine();
+        Console.WriteLine("Hello, {0}!", guest.Name);
+
     }
 }
